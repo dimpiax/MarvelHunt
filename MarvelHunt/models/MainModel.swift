4029fa68de3b4999ccbaa6250b163cb64c1cf329
Dmytro Pylypenko <dimpiax@gmail.com>
2019-09-17 11:08:25 +0200

    Move `ImageFetcher` to services.

--- MarvelHunt/models/MainModel.swift
+++ MarvelHunt/models/MainModel.swift
@@ -7,54 +7,11 @@
 //
 
 import Foundation
-import UIKit
 
 class MainModel {
   let kvStorage = KeyValueStorage()
   
   let serverModel = ServerModel()
   
-  lazy var imageFetcher = ImageManager(serverModel: serverModel)
+  lazy var imageFetcher = ImageFetcher(serverModel: serverModel)
 }
-
-class ImageManager {
-  private let _serverModel: ServerModel
-  
-  init(serverModel: ServerModel) {
-    _serverModel = serverModel
-  }
-  
-  func getImage(request: URLRequest) -> UIImage? {
-    guard
-      let data = URLCache.shared.cachedResponse(for: request)?.data,
-      let image = UIImage(data: data)
-      else {
-        return nil
-    }
-    
-    return image
-  }
-  
-  func getImage(url: URL) -> UIImage? {
-    return getImage(request: URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad, timeoutInterval: 5))
-  }
-  
-  func requestImage(url: URL, completion: @escaping (UIImage) -> Void) -> URLSessionDataTask? {
-    let request = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad, timeoutInterval: 5)
-    if let _ = getImage(request: request) {
-      //completion(image)
-      return nil
-    }
-    
-    let task = _serverModel.requestImage(with: request) { result in
-      guard let (image, cacheData) = try? result.get() else { return }
-      
-      URLCache.shared.store(cacheData: cacheData)
-      
-      completion(image)
-    }
-    
-    return task
-  }
-}
-
